ca8a31a777af5e0622f138748ad438233ee8c4e3
Xyf0606 <Dominic.xyf@outlook.com>
2024-12-28 18:06:09 +0800

    formatting

--- src/lab7-I.cpp
+++ src/lab7-I.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
-class Sort {
+class Sort
+{
 protected:
     std::vector<int> data;
 
@@ -12,11 +13,13 @@ public:
     Sort(int arr[], int n) : data(arr, arr + n) {}
 
     // 默认升序排序
-    virtual void sort(bool reverse = false) {
+    virtual void sort(bool reverse = false)
+    {
         introsort(data.begin(), data.end(), 2 * log2(data.size()), reverse);
     }
 
-    int get(int index) const {
+    int get(int index) const
+    {
         if (index >= 0 && index < data.size()) {
             return data[index];
         }
@@ -27,16 +30,17 @@ public:
 
 protected:
     template <typename RandomIt>
-    void introsort(RandomIt first, RandomIt last, int depth_limit, bool reverse) {
+    void introsort(RandomIt first, RandomIt last, int depth_limit, bool reverse)
+    {
         while (last - first > 16) {
             if (depth_limit == 0) {
-                heapsort(first, last, reverse); // 超过递归深度限制时使用堆排序
+                heapsort(first, last, reverse);  // 超过递归深度限制时使用堆排序
                 return;
             }
             --depth_limit;
             RandomIt pivot = median_of_three(first, first + (last - first) / 2, last - 1, reverse);
             pivot = partition(first, last, *pivot, reverse);
-            if (pivot - first < last - pivot) { // 优先排序较小的区间
+            if (pivot - first < last - pivot) {  // 优先排序较小的区间
                 introsort(first, pivot, depth_limit, reverse);
                 first = pivot + 1;
             } else {
@@ -44,12 +48,13 @@ protected:
                 last = pivot;
             }
         }
-        insertion_sort(first, last, reverse); // 小区间使用插入排序
+        insertion_sort(first, last, reverse);  // 小区间使用插入排序
     }
 
     // 插入排序
     template <typename RandomIt>
-    void insertion_sort(RandomIt first, RandomIt last, bool reverse) {
+    void insertion_sort(RandomIt first, RandomIt last, bool reverse)
+    {
         for (RandomIt i = first + 1; i < last; ++i) {
             auto key = *i;
             RandomIt pos = reverse ? std::upper_bound(first, i, key, std::greater<int>())
@@ -61,7 +66,8 @@ protected:
 
     // 堆排序实现
     template <typename RandomIt>
-    void heapsort(RandomIt first, RandomIt last, bool reverse) {
+    void heapsort(RandomIt first, RandomIt last, bool reverse)
+    {
         if (reverse) {
             std::make_heap(first, last, std::less<int>());
             std::sort_heap(first, last, std::less<int>());
@@ -73,7 +79,8 @@ protected:
 
     // 快速排序分区操作
     template <typename RandomIt, typename T>
-    RandomIt partition(RandomIt first, RandomIt last, T pivot, bool reverse) {
+    RandomIt partition(RandomIt first, RandomIt last, T pivot, bool reverse)
+    {
         RandomIt left = first;
         RandomIt right = last - 1;
         while (true) {
@@ -93,42 +100,57 @@ protected:
 
     // 三点中值法选择枢轴
     template <typename RandomIt>
-    RandomIt median_of_three(RandomIt a, RandomIt b, RandomIt c, bool reverse) {
+    RandomIt median_of_three(RandomIt a, RandomIt b, RandomIt c, bool reverse)
+    {
         if (reverse) {
             if (*a > *b) {
-                if (*b > *c) return b;
-                else if (*a > *c) return c;
-                else return a;
+                if (*b > *c)
+                    return b;
+                else if (*a > *c)
+                    return c;
+                else
+                    return a;
             } else {
-                if (*a > *c) return a;
-                else if (*b > *c) return c;
-                else return b;
+                if (*a > *c)
+                    return a;
+                else if (*b > *c)
+                    return c;
+                else
+                    return b;
             }
         } else {
             if (*a < *b) {
-                if (*b < *c) return b;
-                else if (*a < *c) return c;
-                else return a;
+                if (*b < *c)
+                    return b;
+                else if (*a < *c)
+                    return c;
+                else
+                    return a;
             } else {
-                if (*a < *c) return a;
-                else if (*b < *c) return c;
-                else return b;
+                if (*a < *c)
+                    return a;
+                else if (*b < *c)
+                    return c;
+                else
+                    return b;
             }
         }
     }
 };
 
-class ExSort : public Sort {
+class ExSort : public Sort
+{
 public:
     ExSort(int arr[], int n) : Sort(arr, n) {}
 
     virtual ~ExSort() = default;
 
-    void exsort(int order) {
-        if (order == 0) { // 升序排序
+    void exsort(int order)
+    {
+        if (order == 0) {  // 升序排序
             sort(false);
-        } else { // 降序排序
-            sort(true); // 使用父类提供的内省排序算法，控制排序方向
+        } else {         // 降序排序
+            sort(true);  // 使用父类提供的内省排序算法，控制排序方向
         }
     }
 };
